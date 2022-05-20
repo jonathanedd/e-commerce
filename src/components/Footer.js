be54d03e47acb4d2e8b0151a2828951b5b95934c
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2022-05-19 22:47:57 -0500

    Added social follow links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import '../styles/footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faInstagram,
+  faLinkedinIn,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <div className="footer">
       <h3>Jhonatan Ordonez</h3>
+
+      <div className="social-follow">
+        <ul>
+          <li>
+            <a href="https://github.com/jonathanedd">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/jonaedd/">
+              <FontAwesomeIcon icon={faLinkedinIn} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/jonaedd/">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </li>
+        </ul>
+      </div>
+
       <h4>Academlo 2022</h4>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,115 +1,93 @@
 // import axios from 'axios';
 import React, { useState } from 'react';
-import {FontAwesomeIcon} from  '@fortawesome/react-fontawesome';
-import {faUser, faCartShopping} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { useDispatch } from 'react-redux';
 import { loginThunk, purchasesCartThunk } from '../redux/actions';
 import { Purchases } from '../pages';
 
 const NavBar = () => {
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+  const [isPurchasesOpen, setIsPurchasesOpen] = useState(false);
 
-    const [ isLoginOpen, setIsLoginOpen ] = useState(false);
-    const [ isPurchasesOpen, setIsPurchasesOpen ] = useState(false);
+  // Input control states
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    // Input control states
-    const [ email, setEmail ] = useState('');
-    const [ password, setPassword ] = useState('');
+  const [loginErr, setLoginErr] = useState('');
 
-    const [ loginErr, setLoginErr ] = useState('');
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const openPurchaseCart = () => {
+    setIsPurchasesOpen(!isPurchasesOpen);
+    dispatch(purchasesCartThunk());
+  };
 
-    const openPurchaseCart = () => {
-        setIsPurchasesOpen(!isPurchasesOpen);
-        dispatch(purchasesCartThunk())
-    }
+  //Login button functionality
+  const submit = e => {
+    e.preventDefault();
+    const credentials = { email, password };
+    dispatch(loginThunk(credentials))
+      .then(res => {
+        localStorage.setItem('token', res.data.data.token);
+        setLoginErr('');
+        setIsLoginOpen(false);
+      })
+      .catch(err => {
+        setLoginErr(err.response.data.message);
+      });
+  };
 
-    //Login button functionality
-    const submit = (e) => {
-        e.preventDefault();
-        const credentials = {email, password }
-        dispatch(loginThunk(credentials))
-            .then( res =>{ 
-                localStorage.setItem('token', res.data.data.token)
-                setLoginErr('');
-                setIsLoginOpen(false);
-                
-            })
-            .catch( err => {
-                setLoginErr(err.response.data.message);
-            })
-            
-    }
+  return (
+    <div className="nav-bar">
+      <nav className="mini-nav">
+        <strong>e-commerce</strong>
+        <FontAwesomeIcon
+          className="icon"
+          icon={faUser}
+          onClick={() => setIsLoginOpen(!isLoginOpen)}
+        />
 
-    
+        <FontAwesomeIcon
+          className="icon-purchases"
+          icon={faCartShopping}
+          onClick={openPurchaseCart}
+        />
+      </nav>
 
+      <form onClick={submit} className={`login ${isLoginOpen ? 'open' : ' '}`}>
+        {localStorage.getItem('token') ? (
+          <button
+            onClick={() => localStorage.setItem('token', '')}
+            type="button"
+          >
+            Log out
+          </button>
+        ) : (
+          <>
+            <p>Email</p>
+            <input
+              type="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              placeholder="example@email.com"
+            />
+            <p>Password</p>
+            <input
+              type="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              placeholder="********"
+            />
+            <p>{loginErr}</p>
+            <button>Login</button>
+          </>
+        )}
+      </form>
 
-    return (
-        <div className='nav-bar'>
-
-            <nav className='mini-nav'>
-                <strong>e-commerce</strong>
-                <FontAwesomeIcon 
-                    className='icon' 
-                    icon={faUser} 
-                    onClick={() => setIsLoginOpen(!isLoginOpen)} 
-                />
-
-                <FontAwesomeIcon 
-                    className='icon-purchases' 
-                    icon={faCartShopping}
-                    onClick={openPurchaseCart}
-                />
-                
-                
-            </nav>
-            
-
-
-            
-             
-            <form onClick={submit} className={`login ${isLoginOpen ? 'open' : ' '}`}>
-
-                {
-                    localStorage.getItem('token') ? (
-                        <button onClick={() => localStorage.setItem('token', "")} type='button'>
-                            Log out
-                        </button>
-                    ) : (
-
-                        <>
-                            <p>Email</p>
-                            <input 
-                                type="email" 
-                                value={email} 
-                                onChange={(e) => 
-                                setEmail(e.target.value)} 
-                                placeholder='example@email.com'
-                            />
-                            <p>Password</p>
-                            <input 
-                                type="password" 
-                                value={password} 
-                                onChange={(e) => 
-                                setPassword(e.target.value)} 
-                                placeholder='********' 
-                            />
-                            <p>{ loginErr }</p>
-                            <button>Login</button>
-                        </>
-                        
-                    )
-                }
-                
-            </form>
-
-
-            
-            
-            <Purchases isOpen={isPurchasesOpen} />
-            
-        </div>
-    );
+      <Purchases isOpen={isPurchasesOpen} />
+    </div>
+  );
 };
 
 export default NavBar;
